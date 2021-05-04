1aefda8c6411e6c46fc977820a18e8193f68e7df
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-05-04 17:02:44 -0400

    Feature: Finished Part 3

--- flask_jinja_tutorial/routes.py
+++ flask_jinja_tutorial/routes.py
@@ -1,8 +1,28 @@
 """Route Declaration."""
 
-from flask import current_app as app
-from flask import render_template
+from flask import (
+    Flask,
+    url_for,
+    render_template,
+    redirect
+)
+from .forms import ContactForm
 
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Standard `Contact` Form"""
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect(url_for("success"))
+    return render_template(
+        "contact.jinja2",
+        form=form,
+        template="form-template"
+    )
 @app.route("/")
 def home():
     """Landing Page"""
