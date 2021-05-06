cf13505cc6519f2ba6bad55f33d278bd5ed8bd45
Orville Dillon <60524259+eLEPHANTCase@users.noreply.github.com>
2021-05-06 09:28:44 -0400

    Feature: Finished Part 8

--- flask-blueprint-tutorial/__init__.py
+++ flask-blueprint-tutorial/__init__.py
@@ -1,27 +1,18 @@
-""" Initialize Flask App """
 from flask import Flask
-from flask_assets import Environment # <- `Environment`?
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    """ Create Flask Application """
+db = SQLAlchemy()
+
+
+def init_app():
+    """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
-    assets = Environment() # Create an assets environment
-    assets.init_app(app) # Initialize Flask-Assets
+
+    db.init_app(app)
 
     with app.app_context():
-        # Import Parts of Our Application
-        from .profile import routes
-        from .home import routes
-        from .products import routes
-        from .assets import compile_static_assets
-
-        # Register Blueprints
-        app.register_blueprint(profile.account_bp)
-        app.register_blueprint(home.home_bp)
-        app.register_blueprint(products.product_bp)
-
-        # Compile Static Assets
-        compile_static_assets(assets) # Execute Logic
+        from . import routes  # Import routes
+        db.create_all()  # Create sql tables for our data models
 
         return app
